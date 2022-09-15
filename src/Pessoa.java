355601d0629642f87d93c03fb50f6a1e6df6d05c
astergabedev <gabiferpub@gmail.com>
2022-09-15 14:03:26 -0300

    pessoa class setup

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,2 +1,48 @@
-package PACKAGE_NAME;public class Pessoa {
+public class Pessoa {
+
+    private static int id;
+
+    private String nome;
+    private String cpf;
+    private String email;
+
+
+    public Pessoa(String nome, String cpf, String email){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        id++;
+    }
+
+    public static int getId() {
+        return id;
+    }
+
+    public static void setId(int id) {
+        Pessoa.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
