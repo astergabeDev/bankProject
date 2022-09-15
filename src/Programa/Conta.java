f9ab97c9e11291d8080ef873ffbb80435841022f
astergabedev <gabiferpub@gmail.com>
2022-09-15 14:22:27 -0300

    added conta stuff

--- src/Programa/Conta.java
+++ src/Programa/Conta.java
@@ -2,6 +2,48 @@ package Programa;
 
 public class Conta {
 
+    private static int idConta = 1;
 
+    private int numeroConta;
+    private Pessoa pessoa;
+    private double saldo = 0;
 
+    public Conta(int numeroConta, Pessoa pessoa, double saldo) {
+        this.numeroConta = numeroConta;
+        this.pessoa = pessoa;
+        this.saldo = saldo;
+        idConta++;
+    }
+
+    public static int getIdConta() {
+        return idConta;
+    }
+
+    public static void setIdConta(int idConta) {
+        Conta.idConta = idConta;
+    }
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
 }
